ad2840c289c31d89d7c1eda40f8215d2109af588
Abdrakhmanov Mukhamedkarim <j3t1x0h@gmail.com>
2023-04-06 21:10:46 +0600

    0604

--- src/kz/kstu/cryptography/coursework/Hash.java
+++ src/kz/kstu/cryptography/coursework/Hash.java
@@ -7,6 +7,7 @@ package kz.kstu.cryptography.coursework;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
@@ -16,11 +17,14 @@ import javax.swing.*;
  * @author 1234
  */
 public class Hash extends javax.swing.JFrame {
+    int v =1;
+    int P=0,Q=0,N=0;
 JFileChooser jfc = null;
 File file=null;
 String text = null;
 Path filePath = null;
 String path=null;
+String textgo=" 123";
     /**
      * Creates new form Hash
      */
@@ -43,6 +47,14 @@ String path=null;
         Answer = new javax.swing.JTextField();
         FindButton = new javax.swing.JButton();
         FileButton = new javax.swing.JButton();
+        PTF = new javax.swing.JTextField();
+        QTF = new javax.swing.JTextField();
+        NTF = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        WhereGo = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -66,33 +78,86 @@ String path=null;
             }
         });
 
+        PTF.setEnabled(false);
+
+        QTF.setEnabled(false);
+
+        NTF.setEnabled(false);
+
+        jLabel1.setText("P =");
+
+        jLabel2.setText("Q=");
+
+        jLabel3.setText("N=");
+
+        WhereGo.setText("Куда выводить");
+        WhereGo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                WhereGoActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Значение Hash =");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(50, 50, 50)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(NTF, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(QTF, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(PTF, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(39, 39, 39)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(FindButton)
-                        .addGap(51, 51, 51)
-                        .addComponent(Answer, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(FileButton))
-                .addContainerGap(136, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, Short.MAX_VALUE)
+                        .addComponent(Answer, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(20, 20, 20))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(FileButton)
+                            .addComponent(WhereGo))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(380, Short.MAX_VALUE)
+                .addGap(34, 34, 34)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(PTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(QTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(NTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 203, Short.MAX_VALUE)
+                .addComponent(WhereGo)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(FileButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(Answer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(FindButton)))
+                            .addComponent(FindButton)
+                            .addComponent(jLabel4)))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(34, 34, 34))
         );
@@ -129,8 +194,43 @@ String path=null;
     }//GEN-LAST:event_FileButtonActionPerformed
 
     private void FindButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_FindButtonActionPerformed
-Answer.setText(text);      // TODO add your handling code here:
+char[] charray = text.toCharArray();
+P=OTT.P(1000);
+Q=OTT.P(1000);
+N=P*Q;
+for(int i = 0;i<charray.length;i++){
+    v = ((int) (Math.pow(v^charray[i],2)))%N;
+    textgo =textgo +i+") "+ Integer.toString(v)+"\n";
+}
+PTF.setText(Integer.toString(P));
+QTF.setText(Integer.toString(Q));
+NTF.setText(Integer.toString(N));
+Answer.setText(Integer.toString(v));
     }//GEN-LAST:event_FindButtonActionPerformed
+
+    private void WhereGoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_WhereGoActionPerformed
+        jfc = new JFileChooser();
+       jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+       int ret = jfc.showDialog(null, "Открыть файл");                
+                if (ret == JFileChooser.APPROVE_OPTION) {
+                    file = jfc.getSelectedFile();
+                }
+              try {
+                   path = file.getPath();
+                filePath = Path.of(path);
+            // Write content to file
+            Files.writeString(filePath, textgo,
+                              StandardOpenOption.APPEND);
+  
+            // Verify file content
+            String content = Files.readString(filePath);
+  
+            System.out.println(content);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_WhereGoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -172,6 +272,14 @@ Answer.setText(text);      // TODO add your handling code here:
     private javax.swing.JButton FileButton;
     private javax.swing.JButton FindButton;
     private javax.swing.JTextArea MessageA;
+    private javax.swing.JTextField NTF;
+    private javax.swing.JTextField PTF;
+    private javax.swing.JTextField QTF;
+    private javax.swing.JButton WhereGo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
